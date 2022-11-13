c36627acdb9e9677e3c318342ee8f6de369c6912
Aliya <bugaga789@yandex.ru>
2022-11-13 15:12:03 +0300

    02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp,defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+    name: 'App',
+    data() {
+        return {
+            fst_num: 1,
+            sec_num: 1,    
+            operation: 'init' 
+        };
+      },  
+    methods: {
+        calc(operator){
+            if( operator == 'sum') {
+                return this.fst_num + this.sec_num;
+            }
+            else if ( operator=='subtract' ){
+                return this.fst_num - this.sec_num;
+            } 
+
+            else if( operator == 'multiply')   {
+                return this.fst_num * this.sec_num;
+            }  
+
+            else if( operator == 'divide')   {
+                return this.fst_num / this.sec_num;
+            }
+        }
+    } ,
+
+    computed:{        
+        output() {
+            return this.calc(this.operation);
+        }
+    }  
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
