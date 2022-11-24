8a7b8ce78fe189425e3420f022d4721281840356
Aliya <bugaga789@yandex.ru>
2022-11-24 22:09:59 +0300

    06-MeetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,6 +1,12 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupAgendaItem from '../../05-MeetupAgenda/components/MeetupAgendaItem.js';
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda.js';
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription.js';
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js';
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js';
+
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +14,12 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupAgendaItem,
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupInfo,
+    MeetupCover
+
   },
 
   props: {
@@ -20,19 +32,24 @@ export default defineComponent({
   template: `
     <div>
       <!-- meetup cover-->
+      <MeetupCover :title="meetup.title" :image="meetup.image"/>
 
-      <ui-container>
+      <ui-container >
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
             <!-- meetup description -->
-
+            <MeetupDescription :description="meetup.description"/>
             <h3>Программа</h3>
             <!-- meetup agenda -->
-            <ui-alert>Программа пока пуста...</ui-alert>
+            <template v-if="meetup.agenda.length">
+              <MeetupAgenda  :agenda="meetup.agenda"/>
+            </template>  
+            <ui-alert v-else>Программа пока пуста...</ui-alert>
           </div>
           <div class="meetup__aside">
             <!-- meetup info -->
+            <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date"/>
           </div>
         </div>
       </ui-container>
