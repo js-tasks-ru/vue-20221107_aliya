3e2042e3f72b1ca58ed5497db23f42147a466b41
Aliya <bugaga789@yandex.ru>
2022-12-03 20:45:07 +0300

    04-02-TheToaster_3

---
+++ 04-vue-cli/02-TheToaster/components/TheToast.vue
@@ -0,0 +1,28 @@
+<template >
+   <div class="toast" :class="classToast">
+      <ui-icon class="toast__icon" :icon="iconToast" />
+      <span>{{ textToast }}</span>
+    </div>
+</template> 
+
+<script>
+import UiIcon from './UiIcon.vue';
+
+export default {
+  name: 'TheToast',
+
+  props:{
+    classToast:{
+      type:String
+    },
+    iconToast:{
+      type:String
+    },
+    textToast:{
+       type:String  
+    }
+  },
+
+  components: { UiIcon }
+};
+</script>
